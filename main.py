faab186ccae861b1051651935faa1ec783936d57
Steve Wong <swong420@insite.4cd.edu>
2023-06-13 08:33:33 -0700

    C57: Python Programming

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def main():
-
-    ##############################
-    # make your code below
-    # print('Hello World')
-    ##############################
+    print('Hello World')
+    print('C57: Python Programming')
 
 
 if __name__ == '__main__':
